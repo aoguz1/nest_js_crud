e3e17ed06325f637b54745f15d06229c7ba7c9be
Abdullah Oğuz <abdullah.oguz@mdpgroup.com>
2022-02-17 19:51:31 +0300

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      // port: 3306,
+      username: 'postgres',
+      password: 'admin',
+      database: 'nest_crud',
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/user/user.entity.ts
@@ -0,0 +1,16 @@
+import { ApiProperty, ApiResponse } from '@nestjs/swagger';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @ApiProperty()
+  @Column()
+  firstName: string;
+
+  @ApiProperty()
+  @Column()
+  lastName: string;
+}
